64133c8a9df20aedce44513d0bad9c7759ae1dc9
Damon Morris <d.j.morris@sheffield.ac.uk>
2021-06-21 16:39:59 +0100

    add calculation of local authority level dividend to the workflow

--- R/analysis.R
+++ R/analysis.R
@@ -48,3 +48,16 @@ mean_spend_la <- mean_spend_la[order(UTLAname)]
 ## (4) Grab the income data
 
 income <- smkfreediv::GetIncome(income_var = 3)
+
+## (5) Use the output of (3) and (4) to calculate the smoke free dividend for
+##     each local authority.
+
+div_la <- CalcDividend_la(profiles = smkfreediv::tobacco_profiles,
+                          clean_income = income,
+                          clean_expenditure = mean_spend_la,
+                          upshift = 1.57151042,
+                          div = 0.93,
+                          reps = 200,
+                          seed = 2021)
+
+
