c4090f89818875ecbab992942bc571cde0ca1f11
Damon Morris <d.j.morris@sheffield.ac.uk>
2021-09-17 14:35:38 +0100

    simplify

--- R/001 - package installation.R
+++ R/001 - package installation.R
@@ -1,18 +1,6 @@
 ### Install the smkfreediv package
 
-# obtain the currently installed version of the package
-
-if (!("smkfreediv" %in% installed.packages() )) {
-  installed_version = "NULL"
-} else {
-  installed_version = as.character(packageVersion("smkfreediv")[1])
-  detach("package:smkfreediv")
-}
-
-
-# install the required version, if this is not the one which is installed
-
-if (!(installed_version == version)) {
+# install the required version
 
 devtools::install_git(
   "https://gitlab.com/SPECTRUM_Sheffield/r-packages/smkfreediv.git",
@@ -21,4 +9,4 @@ devtools::install_git(
   build_vignettes = TRUE
 )
 
-}
+
