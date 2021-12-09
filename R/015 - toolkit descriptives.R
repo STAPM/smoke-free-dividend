489be33b629c50934afc2352a4b759be76d4b6ba
Damon Morris <d.j.morris@sheffield.ac.uk>
2021-12-09 16:32:22 +0000

    examine distribution of spending in toolkit data

--- R/015 - toolkit descriptives.R
+++ R/015 - toolkit descriptives.R
@@ -1,19 +1,39 @@
-# calculate mean weekly expenditure by age/sex
-exp <- smkfreediv::CalcWeekSpend(data = data,
-                                 strat_vars = c("Sex","Ageband"),
-                                 upshift = 1)
 
-# get population weights and merge
-pop_n <- smkfreediv::pop_counts
+## produce summary statistics and descriptive plots of the
+## toolkit data
 
-pop_age_sex <- pop_n[, .(pop = sum(population)),
-                     by = c("Sex","Ageband")]
+data <- readRDS(paste0(Dir[1],"/toolkit_clean.rds"))
+data <- data[!(is.na(gor)),]
 
-merge <- merge(exp, pop_age_sex, by = c("Sex","Ageband"), sort = T)
+#### Distribution of weekly spending
 
-ggplot(merge) +
-  aes(x = Ageband, y = mean_week_spend, fill = Sex) +
-  geom_bar(stat = "identity", position = "dodge") +
+exp <- smkfreediv::CalcWeekSpend(data, strat_vars = NULL, upshift = 1)
+
+med <- as.numeric(exp[,"median_week_spend"])
+mean   <- as.numeric(exp[,"mean_week_spend"])
+
+ggplot(data) +
+  aes(x = weekspend) +
+  geom_density(alpha = 0.2) +
+  geom_vline(xintercept = mean, color = "navy", linetype = 2) +
+  geom_vline(xintercept = med, color = "maroon", linetype = 2) +
   theme_minimal() +
-  theme(legend.position = "bottom") +
-  labs(y = "Meaan Weekly Expenditure (£)", x = "Age")
+  scale_fill_viridis_d(option = "mako") +
+  labs(y = " ", x = "Weekly Tobacco Spending (£)",
+       caption = paste0("Median Weekly Spend = £",round(med,2),". Mean Weekly Spend = £",round(mean,2),"."))
+ggsave("output/descriptives/spending distribution.png")
+
+#### Distribution of weekly spending by region
+
+ggplot(data) +
+  aes(x=reorder(gor,-weekspend, FUN = "median", na.rm = TRUE),
+      y = weekspend,
+      fill = gor) +
+  theme_minimal() +
+  coord_flip() +
+  geom_boxplot() +
+  scale_fill_viridis_d(option = "mako") +
+  theme(legend.position = "none") +
+  labs(x = " ", y = "Weekly Tobacco Spending (£)")
+ggsave("output/descriptives/spending distribution by region.png")
+
