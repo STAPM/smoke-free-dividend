73f00d23763b92b7a0098eff56f35aaa1bf0b8f2
Damon Morris <d.j.morris@sheffield.ac.uk>
2021-12-15 10:14:32 +0000

    update output and paper draft

--- R/000 - create directories.R
+++ R/000 - create directories.R
@@ -8,10 +8,10 @@ if(!dir.exists(here::here(intdatDir))) {dir.create(here::here(intdatDir))}
 outputDir <- "output"
 if(!dir.exists(here::here(outputDir))) {dir.create(here::here(outputDir))}
 
-outlaDir <- "output/local authority"
+outlaDir <- "output/comparisons"
 if(!dir.exists(here::here(outlaDir))) {dir.create(here::here(outlaDir))}
 
-outgorDir <- "output/government office region"
+outgorDir <- "output/descriptives"
 if(!dir.exists(here::here(outgorDir))) {dir.create(here::here(outgorDir))}
 
 Dir <- c(rawdatDir,intdatDir,
