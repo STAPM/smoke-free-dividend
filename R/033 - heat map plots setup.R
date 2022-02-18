627c178424f0339b76abb9e224a1e7c65a5c30d6
Damon Morris <d.j.morris@sheffield.ac.uk>
2022-02-18 11:56:47 +0000

    separate setup from the plots in the heat maps code

--- R/033 - heat map plots setup.R
+++ R/033 - heat map plots setup.R
@@ -84,85 +84,9 @@ setDT(shape)
 ### Merge to the full shapefile, note all.y = F to remove Scottish/Welsh LAs
 
 
-merge <- merge(data_mapping_merge, shape, by.x = "LAcode", by.y = "id", all.y = F)
-merge <- arrange(merge,order)
+heat_map_data <- merge(data_mapping_merge, shape, by.x = "LAcode", by.y = "id", all.y = F)
+heat_map_data <- arrange(merge,order)
 
 
 
 #### ---------------------------------------------------------------------------------------------------------------------
-
-#######################
-### plot income #######
-
-hm1 <- ggplot(data = merge) +
-  aes(x = long,
-      y = lat,
-      group = group,
-      fill = inc_decile) +
-  geom_polygon() + coord_equal() + theme_void() +
-  labs(title = '  ',
-       subtitle = ' ',
-       fill = "Decile") +
-  scale_fill_viridis_c(option = "magma") +
-  theme(legend.position = "none")
-
-#####################################
-### plot spend as % of income #######
-
-hm2 <- ggplot(data = merge) +
-  aes(x = long,
-      y = lat,
-      group = group,
-      fill = inc_prop_decile) +
-  geom_polygon() + coord_equal() + theme_void() +
-  labs(title = ' ',
-       subtitle = ' ',
-       fill = "Decile") +
-  scale_fill_viridis_c(option = "magma") +
-  theme(legend.position = "none")
-
-
-#########################
-### plot dividend #######
-
-hm3 <- ggplot(data = merge) +
-  aes(x = long,
-      y = lat,
-      group = group,
-      fill = dividend_decile) +
-  geom_polygon() + coord_equal() + theme_void() +
-  labs(title = ' ',
-       subtitle = ' ',
-       fill = "Decile") +
-  scale_fill_viridis_c(option = "magma") +
-  theme(legend.position = "none")
-
-###########################
-### plot prevalence #######
-
-hm4 <- ggplot(data = merge) +
-  aes(x = long,
-      y = lat,
-      group = group,
-      fill = prev_decile) +
-  geom_polygon() + coord_equal() + theme_void() +
-  labs(title = ' ',
-       subtitle = ' ',
-       fill = "Decile") +
-  scale_fill_viridis_c(option = "magma") +
-  theme(legend.position = "none")
-
-
-
-##### make the plots
-
-
-ggarrange(hm1, hm2,labels = c("Average Income", "Spend % of Income"), ncol = 2, nrow = 1,  common.legend = TRUE, legend = "bottom")
-ggsave("output/map_income.png")
-
-ggarrange(hm1, hm3,labels = c("Average Income", "Dividend per capita"), ncol = 2, nrow = 1,  common.legend = TRUE, legend = "bottom")
-ggsave("output/map_dividend.png")
-
-ggarrange(hm1, hm4,labels = c("Average Income", "Smoking prevalence"), ncol = 2, nrow = 1)
-ggsave("output/map_prev.png")
-
