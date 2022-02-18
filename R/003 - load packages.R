6e5d46e17c56d17189547bdb7d156f5cafbd7d77
Damon Morris <d.j.morris@sheffield.ac.uk>
2022-02-18 11:55:42 +0000

    add all packages to the 003 script file

--- R/003 - load packages.R
+++ R/003 - load packages.R
@@ -6,3 +6,11 @@ library(ggpubr)     # for combining multiple ggplots
 library(rgeos)      # map packages
 library(rgdal)      # map packages
 library(maptools)   # map packages
+
+library(scales)
+library(here)
+library(stringr)
+library(flextable)
+library(magrittr)
+library(readxl)
+library(openxlsx)
