a6697d780a1fca804f179488ae40a059edd5fe05
Damon Morris <d.j.morris@sheffield.ac.uk>
2022-03-15 11:58:29 +0000

    add custom theme for plots

--- R/003 - load packages.R
+++ R/003 - load packages.R
@@ -14,3 +14,13 @@ library(flextable)
 library(magrittr)
 library(readxl)
 library(openxlsx)
+
+### custom theme for plots
+
+theme_custom <- function() {
+  theme_classic() %+replace%
+    theme(plot.title.position="plot", plot.caption.position="plot",
+          strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)),
+          plot.title=element_text(face="bold", size=rel(1.5), hjust=0,
+                                  margin=margin(0,0,5.5,0)) )
+}
