2d55fd0bb6b347770868674856831ca78c59acfc
Damon Morris <d.j.morris@sheffield.ac.uk>
2023-12-13 13:24:07 +0000

    move the metafile for the project to the top level of the repo

--- run_analyses.R
+++ run_analyses.R
@@ -3,9 +3,11 @@
 
 rm(list = ls())
 
-### ----- (0) Setup -------------- ###
+## specify inputs
 data_file <- "omni174_39.1_65.2cot_31.3a_25.4s_recodes_60.5sa.sav" # Toolkit data file used for the paper
 version <- "1.6.1"            # smkfreediv package version which replicates the paper results (1.6.1 for tobacco control paper)
+
+### ----- (0) Setup -------------- ###
 
 source("R/001 - package installation.R")
 source("R/003 - load packages.R")
@@ -20,6 +22,6 @@ source("R/021 - estimate the upshift.R")
 source("R/022 - average weekly spending.R")
 source("R/023 - local authority and region level analysis.R")
 
-### ----- (4) process results ------- ###
+### ----- (4) process results and write out outputs ------- ###
 source("R/041 - write results tables.R")
 source("R/045 - final figures for the paper.R")
