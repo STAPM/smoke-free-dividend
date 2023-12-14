b52a2c5cae5a4d4e1847e21967c6d86e498075e9
Damon Morris <d.j.morris@sheffield.ac.uk>
2023-12-14 15:40:50 +0000

    update results

--- R/041 - write results tables.R
+++ R/041 - write results tables.R
@@ -2,7 +2,6 @@
 # The aim of this code is to summarise the model data to
 # fit into the summary tables
 
-source("R/002 - create directories.R")
 source("R/003 - load packages.R")
 
 ############################################
@@ -646,8 +645,6 @@ openxlsx::writeData(wb,
                     startRow = 3)
 
 ### --------------- Top 10 Exp % of Income LA Tab -------------- ###
-
-rm(results)
 
 la         <- as.vector(as.matrix(la_results_top[,"UTLAname"]))
 region     <- as.vector(as.matrix(la_results_top[,"gor"]))
